96fbe2b6e23d5af329da4c167cadf39758211434
snehasamuel <snehasamuel297@gmail.com>
2022-05-25 19:16:28 +0530

    Header branch completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import View from './components/View';
 function App() {
   return (
    <>
    <BrowserRouter>
    <Routes>
-     <Route/>
+     <Route path="/" exact element={<Signin/>}/>
+     <Route path="/signup" exact element={<Signup/>}/>
+     <Route path="/view" exact element={<View/>}/>
    </Routes>
    </BrowserRouter>
    </>
